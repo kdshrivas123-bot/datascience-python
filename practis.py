4cd3ef1764fbbd983a84c6ecbe179453482f1a90
koushlendra shrivas <koushlendrashrivas@koushlendras-MacBook-Air.local>
2026-07-16 19:14:56 +0530

    new file

--- practis.py
+++ practis.py
@@ -351,6 +351,156 @@
 # else:
 #     print("else:",counter)
 
+            #   apna collage start frome here
+
+
+# a=float(input("Enter your first value:"))
+# b=float(input("Enter your second value:"))
+# print("avg=",(a+b)/2)
+
+# a=int(input("enter your first number :"))
+# b=int(input("enter your second number: "))
+# # print(a>=b)
+# print(a<b)
+# str="apna collage"
+# print(str[4])
+
+# str="hi$ im an $ or $9879:"
+# print(str.count("$"))
+
+# marks=32
+# if(marks>=90):
+#     print("grade a")
+# elif(marks>70<=85):
+#     print("grade b")
+# elif(marks>=33<=70):
+#     print("grade c")
+# else:
+#     print("fail")
+
+#  quistion1 check the number enter by the user is odd or even
+# num=int(input("enter you number "))
+# if(num%2==0):
+#    print("even")
+# elif(num%2!=0):
+#    print("odd")
+
+
+#    question2b find the largest valu enter by the user
+# num1=int(input("enter your first num"))
+# num2=int(input("enter your second num"))
+# num3=int(input("enter your therd num"))
+# if(num1>num2):
+#     print(num1)
+# elif(num2>num3):
+#     print(num2)
+# else:
+#     print(num3)
+
+# num1=int(input("enter your first num"))
+# if(num1%7==0):
+#     print("this num is multiple of 7")
+# else:
+#     print("not multiple  of 7")
+
+
+# a=int(input("enter a number"))
+# b=int(input("enter b number"))
+# c=int(input("enter c number"))
+# print("avg",(a+b+c)/2)
+
+# name=input("enter your name")
+# print("welcome",name)
+
+# wh=1
+# while wh<=100:
+#     print(wh)
+#     wh-=1
+# h=100
+# while h>=-58:
+#     print(h)
+#     h-=1
+ 
+# m=1
+
+# while m<=10:
+#     print(m*2)
+#     m+=1 
+
+
+# num=[3,43,653,63,74,7,2,884,86]
+# heros=["iron man","thoms","hulk","spider man"]
+# ind=0
+# # while ind <len(heros):
+# while ind <0:
+#     print(heros[ind])
+#     ind+=1
+
+# num=[3,43,653,63,74,7,2,884,86]
+# x=3
+# i=0
+# while i<len(num):
+#     if(num[i] ==x):
+#         print("found",i) 
+#         break   
+#     else:
+#         print("finding ")
+#     i+=1
+
+# i=1
+# while i<=10:
+#     if(i%2==0):
+#         i+=1
+#         continue
+#     print(" this are odd numbers=",i)
+#     i+=1
+
+# j=1
+# while j<=20:
+#     if(j%2 !=0):
+#      j+=1  
+#      continue
+#     print("this is even number",j)
+#     j+=1
+
+# list=["koushlendra ","nilesh","anshuman ","sittu"]
+# for num in list:
+#     print(num)
+
+# list =[12,3,45,64,73,74,5,722,45]
+# x=45
+# ind=0
+# # for val in list:
+# #     print(val)
+# for val in list :
+#     if(val==x):
+#         print("x found",ind)
+#         # break
+#     ind+=1
+   
+# for i in range (100,1,-1):
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # for counter in range(5):
@@ -364,15 +514,443 @@
 # else:
 #     print("else",counter)
 
-blocks=int(input("enter number of blocks:"))
-counter=0
-while(blocks-counter>0):
-    counter+=1
-    blocks=blocks-counter
-print("Hieght of the pyramid:",{counter})
-   
+# blocks=int(input("enter number of blocks:"))
+# counter=0
+# while(blocks-counter>0):
+#     counter+=1
+#     blocks=blocks-counter
+# print("Hieght of the pyramid:",{counter})
+       
+
+# after adding any thing we have to give this comand
+# git add*
+# git commit -m "explain the commit here"
+# git puch
+
+
+# list
+# number=[20,45,56,957,9]
+# print(number)
+# print(type(number))
+
+# print(number[0])
+# print(number[1])
+# print(number[2])
+# print(number[3])
+# print(number[4])
+# # print(number[5])
+
+# number[0]=100
+# print(number)
+
+
+# list
+# list=[3,4,6,8,2,8]
+# print(list)
+# print(f'lenght of list:{len(list)}')
+# list.append(67)
+# print(list)
+# print(f'lenght of list:{len(list)}')
+
+# numbers=[111,4,845,8]
+# print(len(numbers))
+# print(numbers)
+
+# numbers.append(0)
+
+# print(len(numbers))
+# print(numbers)
+
+# numbers.insert(0,7775)
+# print(len(numbers))
+# print(numbers)
+
     
     
+# list=[35,63,764,84,6]
+# for lav in list:
+#     print(lav)
+
+# 
+
+# my_list=[35,63,764,84,6]
+# for lav in range(len(my_list)):
+
+#     print(my_list[lav])
+# l=[10,20,30,40,50,60,70,80,90,100]
+# for i in range(1,11):
+#     l.append(i)
+# print(l)
+
+# list=[10,20,30,40,50,60,70,80,90,100]
+# for val in range(10):
+#   list[val]*=2
+# print(list)
+
+
+# list=[10,20,30,40,50,60,70,80,90,100]
+# # sum of eliment
+# for count in range(len(list)):
+    
+#  print(sum(list))
+
+
+# list=[10,20,30,40,50,60,70,80,90,100]
+# sum=0
+# for index in range(len(list)):
+#     sum+=list[index]
+# print(sum)
+
+# a=20
+# b=10
+# print("a:",a)
+# print("b:",b)
+
+# a,b=b,a
+# print("a:",a)
+# print("b:",b)
+
+
+# my_list=[10,20,40,48,70]
+# print(my_list)
+# my_list[0],my_list[4] = my_list[4],my_list[0]
+# my_list[1],my_list[3] = my_list[3],my_list[1]
+# print(my_list)
+
+
+# Step 1: create an empty list
+# beatles = []
+
+# # Step 2: add John Lennon, Paul McCartney, and George Harrison
+# beatles.append("John Lennon")
+# beatles.append("Paul McCartney")
+# beatles.append("George Harrison")
+
+# lit=[1,2,3,4,5]
+# lit_2=[]
+# add=0
+
+# for number in lit:
+#     add+=number
+#     lit_2.append(add)
+# print(lit)
+# print(lit_2)
+
+# my_list=[8,10,6,2,4]
+# print(my_list)
+# count=0
+# for index1 in range(len(my_list)-1):
+#     for index in range(len(my_list)-1):
+#         if(my_list[index]>my_list[index1]):
+#             my_list[index],my_list[index+1]=my_list[index+1],my_list[index]
+# print(my_list)
+# print("my program has run for :",count,"times")
+            
+# my_list=[8,10,6,9,2,4,5,1,3,7]
+# print(my_list)
+# count=0
+# for index1 in range(len(my_list)-1):
+#     for index in range(len(my_list)-1-index1):
+#         count+=1
+#         if(my_list[index]>my_list[index1]):
+#             my_list[index],my_list[index+1]=my_list[index+1],my_list[index]
+# print(my_list)
+# print("my program has run for :",count,"times")
+
+
+# my_list=[8,10,6,2,4]
+# swapped= True # it's  a littile fake  , we need  it to enter the while loop
+# count=0
+# while swapped:
+#     swapped=False #no swaps for so far
+#     for i in range (len(my_list)-1):
+#         count+=1
+#         if my_list[i]>my_list[i+1]:
+#             swapped=True#a swap occurred
+#             my_list[i],my_list[i+1]=my_list[i+1],my_list[i]
+# print(my_list)
+# print("loops are running for :",count,"times")
+
+# my=[1,2,8,7,6]
+# my.sort()
+# print(my)
+
+# my=[1,2,8,7,6]
+# my.reverse()
+# print(my)
+
+# my_list=[1,4,8,7,3]
+# count=0
+# for i  in range(0,len(my_list)//2):
+#     count+=1
+#     my_list[i],my_list[-1*(i+1)]=my_list[-1*(i+1)],my_list[i]
+# print(my_list)
+
+# # for i in range(0,11,-1):
+# #     print(i)
+
+
+# lst=[1,2,3,4,5,6,7,8,9]
+# print(lst)
+# for index in range(len(lst)//2):
+#         lst[index],lst[(-1*(index+1))]=lst[(-1*(index+1))],   lst[index]
+# print(lst)
+
+# lst=["D","F","a","Z"]
+# lst.sort()
+
+# print(lst)
+# print("A">"a")
+
+# a=3
+# b=1 
+# c=2
+# lst=[a,b,c]
+# lst.sort()
+# print(lst)
+ 
+
+# a="A"
+# b="B"
+
+# c ="C"
+# d =""
+# lst=[a,b,c,d]
+# lst.reverse()
+# print(lst)
+
+# lst_1=[1]
+# lst_2=lst_1
+# lst_1[0]=2
+# print(lst_2)
+
+# lst_1=[1,2,3,4]
+# lst_2=lst_1[0:2]
+# lst_1[0]=2
+# print("lst_2",lst_2)
+# print("lst_1",lst_1)
+
+# my_list=[10,8,6,4,2]
+# new_list=my_list[1:3]
+# print(new_list)
+
+# new_list=my_list[1:-1]
+# print(new_list)
+
+
+# new_list=my_list[-1:1]
+# print(new_list)
+# print(ord("A"))
+
+
+# my_list=[10,20,4,6,0]
+# del my_list
+# print(my_list)
+
+# my_list=[10,20,4,6,0]
+# print(5 in my_list)
+# print(5  not in my_list)
+# print(20 in my_list)
+
+# my_list=[17,3,11,5,1,9,7,15,13]
+# mx=my_list[0]
+# for i in my_list:
+#      if i>mx:
+#         mx=i
+# print(mx)
+
+
+# num=input("inter your number")
+
+# m=1
+# while m<=10:
+#     print(m*2)
+#     m+=1 
+
+# num = int(input("Enter your number: "))
+# m = 1
+# while m <= 10:
+#     print(num * m)
+#     m += 1
+ 
+# list=[]
+# for i in range(31):
+#     if(i%2==0):
+#         list.append(i)
+# print(list)
+
+# my_list=[17,3,9,7,8,10,15,16]
+# largest=my_list[0]
+# for index in range (1,len(my_list)):
+#     if largest<my_list[index]:
+#         largest=my_list[index]
+# print(largest)
+
+# list=[17,3,9,7,8,10,5,15,16]
+
+# for j in range (len(list)):
+#     if list[j]==5:
+#         print(j)
+
+# monthly_salary =float(input("enter your monthly salary"))
+# anunual_salary=monthly_salary*12
+# daily_salary=monthly_salary/30
+# tax=anunual_salary*0.10
+# print(f'annual:Rs.{anunual_salary:,.0f}')
+# print(f'daily:Rs.{daily_salary:,.2f}')
+# print(f'tax:Rs.{tax:,.2f}')
+# print(f'net:Rs.{anunual_salary-tax:,.0f}')
+
+# x=["maruti","BMW"]
+# y=["maruti","BMW"]
+# z= x
+# print(y is x)
+# print (z is x)
+# print(y is not x)
+# print(z is not x)
+
+#split and join
+# csv='rahul, 22, bhopal,engineer'
+# parts=csv.split(',')
+# print("parts:",parts)
+# print(parts[0])
+# rejoined='|'.join(parts)
+# print("rejoined:",rejoined)
+
+
+# # check content
+# print('hello123'.isalnum())
+# print('hello123*'.isalnum())
+# print('8787123'.isdigit())
+# print('python'.isalpha())
+# print(' '.isspace())
+
+# lst=[5,4,3,2,1]
+# print(len(lst))
+# print(lst)
+
+# lst.append(6)
+# print(len(lst))
+# print(lst)
+
+# lst.insert(0,222)
+# print(len(lst))
+# print(lst)
+
+list=[1,2,3,4,5,6,7,8,9,10]
+# for i in list:
+#     if i%2!=0:
+#         print(i)
+        
+# index=0
+# while index<len(list):
+#     print(list[index])
+#     index+=1
+    
+# list=[]
+# indx=1
+# # for indx in range(21,31,):
+# #     list.append(indx)
+# # print(list)
+
+# while indx<11:
+#     list.append(indx)
+#     indx+=1
+# print(list)
+        
+    
+# lst=[1,2,3,45,6,7,9,7,67]
+# for i in range(len(lst)):
+#    lst[i]+=1
+# print(lst) 
+
+
+# lst=[1,2,3,45,6,7,9,7,67,100] 
+# sum=0
+# for i in lst:
+#     sum+=i
+# print(sum)
+
+# a=10
+# b=20
+# # print("a",a)
+# print("b",b)
+
+# print("-------")
+
+# temp=b
+
+# a=b
+# b=temp
+
+# print("a",a)
+# print("b",b)
+
+# a,b=b,a
+
+
+# print("a",a)
+# print("b",b)
+
+# swap
+
+# lst=[1,2,3,45,6,7,9,7,67,100] 
+# print(lst)
+# lst[1],lst[5]=lst[5],lst[1]
+# print(lst)
+
+
+# list=[8,10,6,2,4]
+# print(list)
+# for index in range(len(list)-1):
+#     for index_inner in range (len(list)-1):
+#         if list[index_inner]>list[index_inner+1]:
+#             list[index_inner],list[index_inner+1]=list[index_inner+1,list[index_inner]]
+# # print(list)
+
+# list=[8,10,6,2,4]
+# # list.sort()
+# # print(list)
+# list.reverse()
+# print(list)
+
+
+
+# my_list=[17,3,11,5,4,90,534,5332] 5 on which index
+
+
+# list comprehension
+
+# row=[]
+# for i in range (8):
+#     row.append("white pawn")
+# row=["white_pawn"for i in range (8)]
+# print(row)
+
+# squres=[x**2 for x in range (10)]
+# print(squres)
+
+# # twos=[2** i for i in range (8)]
+# # print(twos)
+
+# odds=[x for x in squres if x % 2!=0]
+# print(odds)
+
+# bord=[]
+# for  i in range(8):
+#     row=["empty" for i in range(8)]
+#     bord.append(row)
+# print(bord)
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ selfprectis.py
@@ -0,0 +1,39 @@
+# lst=[234,24,5,25,6,2,46,524,0]
+# # swaping between two numbers
+# print(len(lst))
+# print(lst)
+# lst[0],lst[8]=lst[8],lst[0]
+# lst[1],lst[7]=lst[7],lst[1]
+# lst[2],lst[6]=lst[6],lst[2]
+# lst[3],lst[5]=lst[5],lst[3]
+# lst[4],lst[4]=lst[4],lst[4]
+# lst[5],lst[3]=lst[3],lst[5]
+# lst[6],lst[2]=lst[2],lst[6]
+
+# print(lst)
+
+# Write a program that prints the numbers from 1 to 10 using a for loop.
+# count=0
+# for index in range(1,11):
+#     count+=1
+#     print(index)
+
+# Write a program that prints all even numbers between 1 and 20.
+# i=0
+# count=0
+# for i in range (1,20):
+#    if i %2==0:
+#      print(i)
+
+# Sum of N NumbersAsk the user for a number $N$. Calculate and print 
+# the sum of all numbers from 1 to $N$. (e.g., if $N=5$, sum is $1+2+3+4+5 = 15$).
+
+sum=0
+
+num=input("enter your chooes number")
+num=input("enter your chooes number")
+for i in num:
+    sum+=num
+print(sum)
+
+
